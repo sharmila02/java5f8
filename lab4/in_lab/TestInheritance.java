8029e7d4fbd670a6af4257010d231f8e0ba6009f
sharmilasiram <66949552+sharmila02@users.noreply.github.com>
2020-12-30 12:45:42 +0530

    added car class, pull to system and load it to update .class files

--- lab4/in_lab/TestInheritance.java
+++ lab4/in_lab/TestInheritance.java
@@ -52,6 +52,7 @@ class AutoMobile
 }
 
 //define a sub class Vehicle that inherits the super class AutoMobile
+//introducing extend keyword
 class Vehicle extends AutoMobile
 {
 	// Added variables
@@ -81,6 +82,7 @@ class Vehicle extends AutoMobile
 	
 	}
 
+    //display the details of vehicle
 	public void display()
 	{
 		super.display();
@@ -89,19 +91,62 @@ class Vehicle extends AutoMobile
 		System.out.println("It's fuel capacity is " + fuel);
 		System.out.println("It has speed " + speed + "Km/hr");
 	}
-		
-	
-
-	
-	
-		
 
 }
 
 
-class Car
+class Car extends Vehicle
 {
+    double price;
+    double totalPrice;
+    
+    public Car(){
+        price = 0.0;
+        totalPrice = 0.0;
+    }
+    
+    public Car(String make, String yearModel, String plateNumber,int numOfWheels, int fuel, int speed, double price){
+        super(make, yearModel, plateNumber, numOfWheels, fuel, speed);
+        this.price = price;
+        
+    }
 
+    public void computeTotalPrice()
+    {
+        String year = yearModel.substring(0, 4);
+        int y = Integer.parseInt(year);
+        
+        if(y >= 2000 && y <= 2020)
+        {
+            if((y >= 2000 && y <= 2010))
+            {
+             totalPrice = price - (price * 0.15);   
+            }
+            else if(y > 2010 && y <= 2018)
+            {
+                totalPrice = price - (price * 0.7); 
+            }
+            else if(y > 2018 && y < 2020)
+            {
+                totalPrice = price - (price * 0.5); 
+            }else{
+                totalPrice = price;
+            }
+        }
+        else{
+            
+            System.out.println("Invalid Model");
+        }
+        
+    }
+    
+    public void display()
+    {
+        super.display();
+        System.out.println("Actual Price: " + price);
+        System.out.println("Invoice of Car after discount: " + totalPrice);
+        
+    }
 
 
 }
@@ -115,8 +160,19 @@ public class TestInheritance
 		String make = sc.nextLine();
 		System.out.println("Enter the year and model of the car:");
 		String yearModel = sc.nextLine();
-		AutoMobile a1 = new AutoMobile(make, yearModel);
-		a1.display();
+	    System.out.println("Enter plate number:");
+	    String plateNumber = sc.nextLine();
+	    System.out.println("Enter the number of wheels:");
+	    int numOfWheels = sc.nextInt();
+	    System.out.println("Enter the fuel:");
+	    int fuel = sc.nextInt();
+	    System.out.println("Enter the speed:");
+	    int speed = sc.nextInt();
+	    System.out.println("Enter the price:");
+	    double price = sc.nextDouble();
+	    Car c = new Car(make, yearModel, plateNumber, numOfWheels, fuel, speed, price);
+	    c.computeTotalPrice();
+	    c.display();
 		
 	}
 }
